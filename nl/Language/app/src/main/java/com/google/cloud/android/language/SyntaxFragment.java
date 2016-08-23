eb86256647cb462b2c5955e8968fd1e32921aee1
Yuichi Araki <yaraki@google.com>
2016-08-23 14:02:29 +0900

    Use FlexboxLayout for syntax

--- nl/Language/app/src/main/java/com/google/cloud/android/language/SyntaxFragment.java
+++ nl/Language/app/src/main/java/com/google/cloud/android/language/SyntaxFragment.java
@@ -16,14 +16,12 @@
 
 package com.google.cloud.android.language;
 
+import com.google.android.flexbox.FlexboxLayout;
 import com.google.cloud.android.language.model.TokenInfo;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,8 +31,7 @@ import android.widget.TextView;
 public class SyntaxFragment extends Fragment {
 
     private static final String ARG_TOKENS = "tokens";
-
-    private TokensAdapter mAdapter;
+    private FlexboxLayout mLayout;
 
     public static SyntaxFragment newInstance() {
         final SyntaxFragment fragment = new SyntaxFragment();
@@ -47,70 +44,33 @@ public class SyntaxFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_syntax, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        RecyclerView list = (RecyclerView) view.findViewById(R.id.list);
-        final Context context = getContext();
-        list.setLayoutManager(new LinearLayoutManager(context));
+        final View view = inflater.inflate(R.layout.fragment_syntax, container, false);
+        mLayout = (FlexboxLayout) view.findViewById(R.id.layout);
         TokenInfo[] tokens = (TokenInfo[]) getArguments().getParcelableArray(ARG_TOKENS);
-        mAdapter = new TokensAdapter(tokens);
-        list.setAdapter(mAdapter);
+        if (tokens != null) {
+            showTokens(tokens);
+        }
+        return view;
     }
 
     public void setTokens(TokenInfo[] tokens) {
-        mAdapter.setTokens(tokens);
+        showTokens(tokens);
         getArguments().putParcelableArray(ARG_TOKENS, tokens);
     }
 
-    private static class ViewHolder extends RecyclerView.ViewHolder {
-
-        public final TextView text;
-        public final TextView label;
-        public final TextView partOfSpeech;
-
-        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.item_token, parent, false));
-            text = (TextView) itemView.findViewById(R.id.text);
-            label = (TextView) itemView.findViewById(R.id.label);
-            partOfSpeech = (TextView) itemView.findViewById(R.id.part_of_speech);
+    private void showTokens(TokenInfo[] tokens) {
+        mLayout.removeAllViews();
+        final LayoutInflater inflater = LayoutInflater.from(getContext());
+        for (TokenInfo token : tokens) {
+            final View view = inflater.inflate(R.layout.item_token, mLayout, false);
+            TextView text = (TextView) view.findViewById(R.id.text);
+            TextView label = (TextView) view.findViewById(R.id.label);
+            TextView partOfSpeech = (TextView) view.findViewById(R.id.part_of_speech);
+            text.setText(token.text);
+            label.setText(token.label != null ? token.label.toLowerCase() : null);
+            partOfSpeech.setText(token.partOfSpeech);
+            mLayout.addView(view);
         }
-
-    }
-
-    private static class TokensAdapter extends RecyclerView.Adapter<ViewHolder> {
-
-        private TokenInfo[] mTokens;
-
-        public TokensAdapter(TokenInfo[] tokens) {
-            mTokens = tokens;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            final TokenInfo token = mTokens[position];
-            holder.text.setText(token.text);
-            holder.label.setText(token.label != null ? token.label.toLowerCase() : null);
-            holder.partOfSpeech.setText(token.partOfSpeech);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mTokens == null ? 0 : mTokens.length;
-        }
-
-        public void setTokens(TokenInfo[] tokens) {
-            mTokens = tokens;
-            notifyDataSetChanged();
-        }
-
     }
 
 }

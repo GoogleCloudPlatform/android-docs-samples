9b371db167007f047abafefa814186cc83e451e8
Yuichi Araki <yaraki@users.noreply.github.com>
2017-03-03 12:09:05 +0900

    Tidy up the implementation (#13)
    ApiFragment and AccessTokenLoader are now merged into SpeechService.

--- speech/Speech/app/src/main/java/com/google/cloud/android/speech/SpeechService.java
+++ speech/Speech/app/src/main/java/com/google/cloud/android/speech/SpeechService.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Google Inc. All Rights Reserved.
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +15,19 @@
  */
 
 package com.google.cloud.android.speech;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.AccessToken;
@@ -28,17 +41,13 @@ import com.google.cloud.speech.v1beta1.StreamingRecognizeRequest;
 import com.google.cloud.speech.v1beta1.StreamingRecognizeResponse;
 import com.google.protobuf.ByteString;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.text.TextUtils;
-import android.util.Log;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -59,15 +68,7 @@ import io.grpc.okhttp.OkHttpChannelProvider;
 import io.grpc.stub.StreamObserver;
 
 
-/**
- * Handles all the API requests of Cloud Speech API.
- *
- * <p>The calling {@link android.app.Activity} needs to implement
- * {@link ApiFragment.Listener}.</p>
- *
- * <p>This is a <em>retained</em> Fragment.</p>
- */
-public class ApiFragment extends Fragment {
+public class SpeechService extends Service {
 
     public interface Listener {
 
@@ -81,16 +82,27 @@ public class ApiFragment extends Fragment {
 
     }
 
-    public static final List<String> SCOPE
-            = Collections.singletonList("https://www.googleapis.com/auth/cloud-platform");
+    private static final String TAG = "SpeechService";
 
+    private static final String PREFS = "SpeechService";
+    private static final String PREF_ACCESS_TOKEN_VALUE = "access_token_value";
+    private static final String PREF_ACCESS_TOKEN_EXPIRATION_TIME = "access_token_expiration_time";
+
+    /** We reuse an access token if its expiration time is longer than this. */
+    private static final int ACCESS_TOKEN_EXPIRATION_TOLERANCE = 30 * 60 * 1000; // thirty minutes
+    /** We refresh the current access token before it expires. */
+    private static final int ACCESS_TOKEN_FETCH_MARGIN = 60 * 1000; // one minute
+
+    public static final List<String> SCOPE =
+            Collections.singletonList("https://www.googleapis.com/auth/cloud-platform");
     private static final String HOSTNAME = "speech.googleapis.com";
-
     private static final int PORT = 443;
 
-    private static final String TAG = "ApiFragment";
-
+    private final SpeechBinder mBinder = new SpeechBinder();
+    private final ArrayList<Listener> mListeners = new ArrayList<>();
+    private volatile AccessTokenTask mAccessTokenTask;
     private SpeechGrpc.SpeechStub mApi;
+    private static Handler mHandler;
 
     private final StreamObserver<StreamingRecognizeResponse> mResponseObserver
             = new StreamObserver<StreamingRecognizeResponse>() {
@@ -106,8 +118,10 @@ public class ApiFragment extends Fragment {
                     text = alternative.getTranscript();
                 }
             }
-            if (text != null && mListener != null) {
-                mListener.onSpeechRecognized(text, isFinal);
+            if (text != null) {
+                for (Listener listener : mListeners) {
+                    listener.onSpeechRecognized(text, isFinal);
+                }
             }
         }
 
@@ -125,53 +139,67 @@ public class ApiFragment extends Fragment {
 
     private StreamObserver<StreamingRecognizeRequest> mRequestObserver;
 
-    private Listener mListener;
+    public static SpeechService from(IBinder binder) {
+        return ((SpeechBinder) binder).getService();
+    }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRetainInstance(true);
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler();
+        fetchAccessToken();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mHandler.removeCallbacks(mFetchAccessTokenRunnable);
+        mHandler = null;
         // Release the gRPC channel.
-        final ManagedChannel channel = (ManagedChannel) mApi.getChannel();
-        if (channel != null && !channel.isShutdown()) {
-            try {
-                channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                Log.e(TAG, "Error shutting down the gRPC channel.", e);
+        if (mApi != null) {
+            final ManagedChannel channel = (ManagedChannel) mApi.getChannel();
+            if (channel != null && !channel.isShutdown()) {
+                try {
+                    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "Error shutting down the gRPC channel.", e);
+                }
             }
+            mApi = null;
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        mListener = (Listener) context;
+    private void fetchAccessToken() {
+        if (mAccessTokenTask != null) {
+            return;
+        }
+        mAccessTokenTask = new AccessTokenTask();
+        mAccessTokenTask.execute();
     }
 
-    @Override
-    public void onDetach() {
-        mListener = null;
-        super.onDetach();
+    private String getDefaultLanguageCode() {
+        final Locale locale = Locale.getDefault();
+        final StringBuilder language = new StringBuilder(locale.getLanguage());
+        final String country = locale.getCountry();
+        if (!TextUtils.isEmpty(country)) {
+            language.append("-");
+            language.append(country);
+        }
+        return language.toString();
     }
 
-    /**
-     * Sets the {@link AccessToken} to be used to call the API.
-     *
-     * @param accessToken The {@link AccessToken}.
-     */
-    public void setAccessToken(AccessToken accessToken) {
-        final ManagedChannel channel = new OkHttpChannelProvider()
-                .builderForAddress(HOSTNAME, PORT)
-                .nameResolverFactory(new DnsNameResolverProvider())
-                .intercept(new GoogleCredentialsInterceptor(new GoogleCredentials(accessToken)
-                        .createScoped(SCOPE)))
-                .build();
-        mApi = SpeechGrpc.newStub(channel);
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public void addListener(@NonNull Listener listener) {
+        mListeners.add(listener);
+    }
+
+    public void removeListener(@NonNull Listener listener) {
+        mListeners.remove(listener);
     }
 
     /**
@@ -227,15 +255,76 @@ public class ApiFragment extends Fragment {
         mRequestObserver = null;
     }
 
-    private String getDefaultLanguageCode() {
-        final Locale locale = Locale.getDefault();
-        final StringBuilder language = new StringBuilder(locale.getLanguage());
-        final String country = locale.getCountry();
-        if (!TextUtils.isEmpty(country)) {
-            language.append("-");
-            language.append(country);
+    private class SpeechBinder extends Binder {
+
+        SpeechService getService() {
+            return SpeechService.this;
         }
-        return language.toString();
+
+    }
+
+    private final Runnable mFetchAccessTokenRunnable = new Runnable() {
+        @Override
+        public void run() {
+            fetchAccessToken();
+        }
+    };
+
+    private class AccessTokenTask extends AsyncTask<Void, Void, AccessToken> {
+
+        @Override
+        protected AccessToken doInBackground(Void... voids) {
+            final SharedPreferences prefs =
+                    getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+            String tokenValue = prefs.getString(PREF_ACCESS_TOKEN_VALUE, null);
+            long expirationTime = prefs.getLong(PREF_ACCESS_TOKEN_EXPIRATION_TIME, -1);
+
+            // Check if the current token is still valid for a while
+            if (tokenValue != null && expirationTime > 0) {
+                if (expirationTime
+                        > System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TOLERANCE) {
+                    return new AccessToken(tokenValue, new Date(expirationTime));
+                }
+            }
+
+            // ***** WARNING *****
+            // In this sample, we load the credential from a JSON file stored in a raw resource
+            // folder of this client app. You should never do this in your app. Instead, store
+            // the file in your server and obtain an access token from there.
+            // *******************
+            final InputStream stream = getResources().openRawResource(R.raw.credential);
+            try {
+                final GoogleCredentials credentials = GoogleCredentials.fromStream(stream)
+                        .createScoped(SCOPE);
+                final AccessToken token = credentials.refreshAccessToken();
+                prefs.edit()
+                        .putString(PREF_ACCESS_TOKEN_VALUE, token.getTokenValue())
+                        .putLong(PREF_ACCESS_TOKEN_EXPIRATION_TIME,
+                                token.getExpirationTime().getTime())
+                        .apply();
+                return token;
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to obtain access token.", e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(AccessToken accessToken) {
+            mAccessTokenTask = null;
+            final ManagedChannel channel = new OkHttpChannelProvider()
+                    .builderForAddress(HOSTNAME, PORT)
+                    .nameResolverFactory(new DnsNameResolverProvider())
+                    .intercept(new GoogleCredentialsInterceptor(new GoogleCredentials(accessToken)
+                            .createScoped(SCOPE)))
+                    .build();
+            mApi = SpeechGrpc.newStub(channel);
+
+            // Schedule access token refresh before it expires
+            mHandler.postDelayed(mFetchAccessTokenRunnable,
+                    Math.max(accessToken.getExpirationTime().getTime() - System.currentTimeMillis()
+                            - ACCESS_TOKEN_FETCH_MARGIN, ACCESS_TOKEN_EXPIRATION_TOLERANCE));
+        }
     }
 
     /**
@@ -249,7 +338,7 @@ public class ApiFragment extends Fragment {
 
         private Map<String, List<String>> mLastMetadata;
 
-        public GoogleCredentialsInterceptor(Credentials credentials) {
+        GoogleCredentialsInterceptor(Credentials credentials) {
             mCredentials = credentials;
         }
 
@@ -264,7 +353,7 @@ public class ApiFragment extends Fragment {
                         throws StatusException {
                     Metadata cachedSaved;
                     URI uri = serviceUri(next, method);
-                    synchronized (GoogleCredentialsInterceptor.this) {
+                    synchronized (this) {
                         Map<String, List<String>> latestMetadata = getRequestMetadata(uri);
                         if (mLastMetadata == null || mLastMetadata != latestMetadata) {
                             mLastMetadata = latestMetadata;

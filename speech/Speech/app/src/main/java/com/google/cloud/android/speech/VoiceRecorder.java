88cea2aab5e33487664558531e64b55d2badd55d
Jeongho Ahn <macro516@gmail.com>
2017-07-05 16:20:19 +0900

    Updated amplitude calculation

--- speech/Speech/app/src/main/java/com/google/cloud/android/speech/VoiceRecorder.java
+++ speech/Speech/app/src/main/java/com/google/cloud/android/speech/VoiceRecorder.java
@@ -213,10 +213,7 @@ public class VoiceRecorder {
         private boolean isHearingVoice(byte[] buffer, int size) {
             for (int i = 0; i < size - 1; i += 2) {
                 // The buffer has LINEAR16 in little endian.
-                int s = buffer[i + 1];
-                if (s < 0) s *= -1;
-                s <<= 8;
-                s += Math.abs(buffer[i]);
+                int s = Math.abs(buffer[i + 1] << 8 | buffer[i]);
                 if (s > AMPLITUDE_THRESHOLD) {
                     return true;
                 }

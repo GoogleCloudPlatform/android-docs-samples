aa4ddd205f908912c0e858c47ad72708c08460ae
Frank Natividad <franknatividad@google.com>
2017-07-12 15:18:26 -0700

    Adding legacy sample and creating v2 version.

---
+++ endpoints-frameworks/legacy/backend/src/main/java/com/example/migration/endpoints/backend/MyEndpoint.java
@@ -0,0 +1,58 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.example.migration.endpoints.backend;
+
+/*
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.endpoints.migration.example.com",
+                ownerName = "backend.endpoints.migration.example.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name + "!");
+
+        return response;
+    }
+
+}

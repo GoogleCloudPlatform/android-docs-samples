721a39c38cff21f76ca110d6fbd7fd0459727a4a
nnegrey <nnegrey@google.com>
2019-09-19 15:38:46 -0600

    Add support to detect intents via audio

--- dialogflow/app/src/main/java/com/google/cloud/examples/dialogflow/utils/ApiRequest.java
+++ dialogflow/app/src/main/java/com/google/cloud/examples/dialogflow/utils/ApiRequest.java
@@ -20,6 +20,8 @@ import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.dialogflow.v2beta1.AudioEncoding;
+import com.google.cloud.dialogflow.v2beta1.InputAudioConfig;
 import com.google.cloud.dialogflow.v2beta1.DetectIntentRequest;
 import com.google.cloud.dialogflow.v2beta1.DetectIntentResponse;
 import com.google.cloud.dialogflow.v2beta1.KnowledgeAnswers;
@@ -35,6 +37,8 @@ import com.google.cloud.dialogflow.v2beta1.SessionsClient;
 import com.google.cloud.dialogflow.v2beta1.SessionsSettings;
 import com.google.cloud.dialogflow.v2beta1.TextInput;
 import com.google.cloud.examples.dialogflow.AppController;
+import com.google.protobuf.ByteString;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -64,23 +68,26 @@ public class ApiRequest {
      * @param knowledge   :   send message to knowledge base if true
      * @return :   response from the server
      */
-    public String callAPI(String accessToken, Date expiryTime, String msg, boolean tts,
+    public String callAPI(String accessToken, Date expiryTime, String msg, byte[] audioBytes, boolean tts,
                           boolean sentiment, boolean knowledge) {
         this.token = accessToken;
         this.tokenExpiration = expiryTime;
-        return detectIntent(msg, tts, sentiment, knowledge);
+
+        return detectIntent(msg, audioBytes, tts, sentiment, knowledge);
     }
 
     /**
      * function to getting the results from the dialogflow
      *
-     * @param msg       :   message sent by the user
-     * @param tts       :   send message to text to speech if true
-     * @param sentiment :   send message to sentiment analysis if true
-     * @param knowledge :   send message to knowledge base if true
+     * @param msg        :   message sent by the user
+     * @param audioBytes :   audio sent by the user
+     * @param tts        :   send message to text to speech if true
+     * @param sentiment  :   send message to sentiment analysis if true
+     * @param knowledge  :   send message to knowledge base if true
      * @return :   response from the server
      */
-    private String detectIntent(String msg, boolean tts, boolean sentiment, boolean knowledge) {
+    private String detectIntent(String msg, byte[] audioBytes, boolean tts, boolean sentiment,
+                                boolean knowledge) {
         try {
             AccessToken accessToken = new AccessToken(token, tokenExpiration);
             Credentials credentials = GoogleCredentials.create(accessToken);
@@ -92,17 +99,30 @@ public class ApiRequest {
             SessionName sessionName = SessionName.of(AppController.PROJECT_ID,
                     AppController.SESSION_ID);
 
-            // Set the text (hello) and language code (en-US) for the query
-            TextInput textInput = TextInput.newBuilder()
-                    .setText(msg)
-                    .setLanguageCode("en-US")
-                    .build();
+            QueryInput queryInput;
+            if (msg != null) {
+                // Set the text (hello) and language code (en-US) for the query
+                TextInput textInput = TextInput.newBuilder()
+                        .setText(msg)
+                        .setLanguageCode("en-US")
+                        .build();
 
-            // Build the query with the TextInput
-            QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+                // Build the query with the TextInput
+                queryInput = QueryInput.newBuilder().setText(textInput).build();
+            } else {
+                // Instructs the speech recognizer how to process the audio content.
+                InputAudioConfig inputAudioConfig = InputAudioConfig.newBuilder()
+                        .setAudioEncoding(AudioEncoding.AUDIO_ENCODING_AMR)
+                        .setLanguageCode("en-US")
+                        .setSampleRateHertz(8000)
+                        .build();
+
+                // Build the query with the TextInput
+                queryInput = QueryInput.newBuilder().setAudioConfig(inputAudioConfig).build();
+            }
 
             DetectIntentRequest detectIntentRequest = getDetectIntentRequest(sessionName,
-                    queryInput, tts, sentiment, knowledge, fixedCredentialsProvider);
+                    queryInput, tts, sentiment, knowledge, fixedCredentialsProvider, audioBytes);
 
             DetectIntentResponse detectIntentResponse =
                     sessionsClient.detectIntent(detectIntentRequest);
@@ -113,7 +133,14 @@ public class ApiRequest {
                 AppController.playAudio(detectIntentResponse.getOutputAudio().toByteArray());
             }
 
-            return handleResults(detectIntentResponse);
+            if (msg != null) {
+                return handleResults(detectIntentResponse);
+            } else {
+                return String.format(
+                        "%s|%s",
+                        handleResults(detectIntentResponse),
+                        detectIntentResponse.getQueryResult().getQueryText());
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
             return ex.getMessage();
@@ -133,11 +160,15 @@ public class ApiRequest {
      */
     private DetectIntentRequest getDetectIntentRequest(SessionName sessionName,
             QueryInput queryInput, boolean tts, boolean sentiment, boolean knowledge,
-            FixedCredentialsProvider fixedCredentialsProvider) throws Exception {
+            FixedCredentialsProvider fixedCredentialsProvider, byte[] audioBytes) throws Exception {
 
         DetectIntentRequest.Builder detectIntentRequestBuilder = DetectIntentRequest.newBuilder()
                 .setSession(sessionName.toString())
                 .setQueryInput(queryInput);
+
+        if (audioBytes != null) {
+            detectIntentRequestBuilder.setInputAudio(ByteString.copyFrom(audioBytes));
+        }
 
         QueryParameters.Builder queryParametersBuilder = QueryParameters.newBuilder();
 
